3a7c2ccd98edb666326101f5dd91483436f0e6d9
fit087 <fitomillennium@hotmail.com>
2014-08-31 12:12:35 -0300

    Rando outros Exemplo
    rodando Kt=0.025

--- MDF.py
+++ MDF.py
@@ -6,10 +6,21 @@ Created on Tue Aug 26 18:56:18 2014
 """
 import math 
 import numpy as np
+
+def main():
+    A=matrizIdent()
+    B=matrizB()
+    resultado=[]
+    resultado.append(B)
+    for i in range(1,21):
+       B=resolve(A,B)
+       resultado.append(B)
+    return  resultado   
+    
 #def main ():
 n=21
 #    A = forme_matriz(n,n)
-kt=0.05
+kt=0.025
 d=0.05
 h=0.1
 u=1.0
@@ -41,13 +52,13 @@ def matrizIdent():
     
 # Condição de Contorno
 def condCont(x):
-    
+
 #    return math.exp(-4*(pi**2))*math.sin(2*pi*k*(x-u*t))
-    return math.exp(-d*(k**2)*t*math.sin(2*pi*k*(x-u*t))
+    return math.exp(-d*(k**2)*t)*math.sin(2*np.pi*k*(2-u*t))
     
 # Condição Inicial
 def condInic(x):
-    return math.sin(2.0*pi*k*(x*1.0))
+    return math.sin(2.0*np.pi*k*(x*1.0))
     
 # Matriz B
 def matrizB():
@@ -56,14 +67,16 @@ def matrizB():
     while x<2.1:
         if x != 0 and x != 2:
             matriz.append(condInic(x))
+#        matriz.append(condCont(x))
         else:
             matriz.append(condCont(x))
         x=x+0.1
+        
     return matriz
     
 # X = A*B
 def resolve(matrizA, matrizB):
     return np.linalg.solve(matrizA, matrizB)
-    
-
-    
+       
+X=main()
+print X
