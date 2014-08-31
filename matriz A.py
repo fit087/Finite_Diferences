f453c3fa95833ee2ba72946cbde7a115dec9758d
fit087 <fitomillennium@hotmail.com>
2014-08-31 10:03:41 -0300

    Correção das Matrizes A e B
    Correção das funções das matrizes A e B

--- matriz A.py
+++ matriz A.py
@@ -5,48 +5,48 @@ Created on Mon Aug 25 20:32:17 2014
 @author: Karyta Almeida
 """
 
-
+import numpy as np
 #def main ():
-n=4
+n=21
 #    A = forme_matriz(n,n)
-#    kt=0.05
-#    D=0.05
-#    h=0.10
-#    u=1
-c=-4.95
-a=-5.05
-b=30
-A=b*np.eye(n, k=0)+(a)*np.eye(n, k=-1)+(c)*np.eye(n, k=1)
-
+kt=0.05
+d=0.05
+h=0.1
+u=1
+   
+A1= (u*kt)/(2*h)+(d*kt)/(h**2)
+A2= (kt/kt)+(2*d*kt)/(h**2)
+A3= (u*kt)/(2*h)-(d*kt)/(h**2)
+A=A2*np.eye(n, k=0)+(A1)*np.eye(n, k=-1)+(A3)*np.eye(n, k=1)
 print A
-    
-
-#def forme_matriz(lin,col):
-#    kt=0.05
-#    D=0.05
-#    h=0.10
-#    u=1
-#    c=-4.95
-#    a=-5.05
-#    b=30
-#
-#
-#
-#    matriz = []
-#    for i in range(lin):
-#        linha = []
-#        for j in range (col):
-#            ele = float(input("Digite o elemento a%d%d da matriz: "%(i+1,j+1)))
-#            linha.append(ele)
-#        matriz.append(linha)
-#    return matriz
-#main ()
 
 
+#aplicando as condições iniciais - matriz B
 
+import math 
+#n=21
+k = 1
+pi = 3.141592653
+t = 0
+d = 0.05
+u = 1
+x = 0
+matriz = []
 
-#30*np.eye(3, k=0)+(-5.05)*np.eye(3, k=-1)+(-4.95)*np.eye(3, k=1)
+while x<=2:
+    coluna = []                
+    b=2*pi*(k**2)*(x-u*t)
+    a = math.exp(-4*(pi**2))*math.sin(b)
+#    c = [[(a) for y in range(1)] for z in range(1)]
+    x = x+0.1
+    ele = float(a)
+    coluna.append(a)
+    matriz.append(coluna)
+    print c
 
+# Ax=B - resolvendo
+B=np.linalg.solve(A, c)
+print B
 
     
 
--- matriz B.py
+++ matriz B.py
@@ -12,13 +12,17 @@ Created on Mon Aug 25 17:35:14 2014
 i = 0
 k = 1
 pi = 3.141592653
+t = 0
+d = 0.05
+u = 1
+
 matriz = []
 
 while i<=2:
     linha = []                
-    b=2*pi*i*k
-    a = math.sin(b)
-#    c = [[(a) for y in range(1)] for x in range(1)]
+    b=2*pi*i*k*(i-u*t)
+    a = math.exp(-d*k*t)*math.sin(b)
+    c = [[(a) for y in range(1)] for x in range(1)]
     i = i+0.1
     ele = float(a)
     linha.append(a)
